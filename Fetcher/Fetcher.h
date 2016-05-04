716e1351c1894f989d33c030459789cfe7cafcee
Andrew Goodale <andrewhgoodale@gmail.com>
2016-05-04 10:49:24 -0400

    Initial Commit

---
+++ Fetcher/Fetcher.h
@@ -0,0 +1,19 @@
+//
+//  Fetcher.h
+//  Fetcher
+//
+//  Created by Andrew on 5/4/16.
+//  Copyright © 2016 Seaview Software LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Fetcher.
+FOUNDATION_EXPORT double FetcherVersionNumber;
+
+//! Project version string for Fetcher.
+FOUNDATION_EXPORT const unsigned char FetcherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fetcher/PublicHeader.h>
+
+
